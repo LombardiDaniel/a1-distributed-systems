6e243523285d3e04846601a516ae13fbb67e1013
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2023-06-19 16:27:09 -0300

    a

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ NÃO tem hadshake, td mundo pode escutar, vc só fala quem vc quer q escute
 """
 
 import logging
+import os
 import sys
 from typing import Iterable
 
@@ -27,31 +28,38 @@ LISTEN_TOPIC_FILTERS = {
     "broadcast": 0,
     "logout": 1
 } # must be int - not unicode
+BROADCAST_TOPIC = b"broadcast"
+
+
+ZMQ_CONTEXT = zmq.Context()
+SEND_SOCKET = ZMQ_CONTEXT.socket(zmq.PUB)  # pylint: disable=E1101
+SEND_SOCKET.bind(f"tcp://*:{DEFAULT_PORT}")
+RECIEVE_SOCKET = ZMQ_CONTEXT.socket(zmq.SUB)  # pylint: disable=E1101
 
 
 def main(*, desigred_dict: Iterable[str]):
     """
     main func loop
     """
-    
 
     logger.info("rcv_usrs_dict : %s", desigred_dict)
 
-    zmq_context = zmq.Context()
-    snd_socket = zmq_context.socket(zmq.PUB)  # pylint: disable=E1101
-    snd_socket.bind(f"tcp://*:{DEFAULT_PORT}")
-    rcv_socket = zmq_context.socket(zmq.SUB)  # pylint: disable=E1101
-
     for ip in desigred_dict.values():  # só se conecta nos usuários que vc quer ouvir
-        rcv_socket.connect(f"tcp://{ip}:{DEFAULT_PORT}")
+        RECIEVE_SOCKET.connect(f"tcp://{ip}:{DEFAULT_PORT}")
 
-    for val in LISTEN_TOPIC_FILTERS.values():
-        rcv_socket.setsockopt(zmq.SUBSCRIBE, val)  # pylint: disable=E1101
+    # for val in LISTEN_TOPIC_FILTERS.values():
+    #     RECIEVE_SOCKET.setsockopt(zmq.SUBSCRIBE, val)  # pylint: disable=E1101
+
+    RECIEVE_SOCKET.setsockopt(zmq.SUBSCRIBE, BROADCAST_TOPIC)
+
+    # TESTS:
+    SEND_SOCKET.send_string(BROADCAST_TOPIC, "eae2")
+    print(RECIEVE_SOCKET.recv_string())
 
 
 if __name__ == "__main__":
 
-    Utils.set_redis_for_tests()
+    # Utils.set_redis_for_tests()
 
     usr_tbl_dict = Utils.get_usrs_tbl()
     available_usrs = {
@@ -62,7 +70,7 @@ if __name__ == "__main__":
 
     VALID_USR_NAME = False
     while not VALID_USR_NAME:
-        usr_name = input("please type your username: ")
+        usr_name = input("Please type your username: ")
 
         if usr_name not in usr_tbl_dict.keys():
             VALID_USR_NAME = True
@@ -72,11 +80,18 @@ if __name__ == "__main__":
     Utils().announce_usr(usr_name)
 
     DESIGNRED_CONN = "-1"
-    while DESIGNRED_CONN == "-1":
+    while DESIGNRED_CONN in ("-1", None, ""):
+
+        usr_tbl_dict = Utils.get_usrs_tbl()
+        available_usrs = {
+            str(i): usr_name for i, usr_name in enumerate(usr_tbl_dict.keys())
+        }
+        os.system("cls||clear")
+        print("Online users:")
         for k, v in available_usrs.items():
             print(f"\t{k}: {v}")
 
-        DESIGNRED_CONN = input("Select the desigred usrs: (type IDs seperated by comma (','))\n(-1 to refresh):\n") #.replace(" ", "").split(",")
+        DESIGNRED_CONN = input("Select the desigred usrs: (type IDs seperated by comma (','))\n(-1 to refresh):\n")
 
     desigred_conn_ids = []
     desigred_conn_ids = DESIGNRED_CONN.replace(" ", "").split(",")
