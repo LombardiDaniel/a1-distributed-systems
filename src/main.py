ed21b6fb52514c2fcc2bb9e33608551928dfc33e
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2023-06-21 12:09:01 -0300

    upgrades ppl, upgrades

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ NÃO tem hadshake, td mundo pode escutar, vc só fala quem vc quer q escute
 import logging
 import os
 import sys
+from threading import Thread
 from typing import Iterable
 
 import zmq
@@ -28,13 +29,34 @@ LISTEN_TOPIC_FILTERS = {
     "broadcast": 0,
     "logout": 1
 } # must be int - not unicode
-BROADCAST_TOPIC = b"broadcast"
+BROADCAST_TOPIC = b""
 
 
 ZMQ_CONTEXT = zmq.Context()
 SEND_SOCKET = ZMQ_CONTEXT.socket(zmq.PUB)  # pylint: disable=E1101
 SEND_SOCKET.bind(f"tcp://*:{DEFAULT_PORT}")
 RECIEVE_SOCKET = ZMQ_CONTEXT.socket(zmq.SUB)  # pylint: disable=E1101
+
+LISTEN_THREADS = []
+
+
+def recv_routine(name: str, ip: str, topic_filter: bytes):
+    """
+    Connects and recieves the data stream
+    """
+
+    endpoint = f"tcp://{ip}:{DEFAULT_PORT}"
+    RECIEVE_SOCKET.connect(f"tcp://{ip}:{DEFAULT_PORT}")
+
+    logger.info("connected to %s @ %s", name, endpoint)
+
+    RECIEVE_SOCKET.setsockopt(zmq.SUBSCRIBE, topic_filter)
+
+    while True:
+        msg = RECIEVE_SOCKET.recv()  # AQUI RECEBE, PRECISA STREAMAR FEED PRA TELA
+        topic, payload = msg.split(b" ", maxsplit=1)
+
+        logger.info("rcvd from topic: '%s'; msg:'%s'", topic.decode("utf-8"), payload.decode("utf-8"))
 
 
 def main(*, desigred_dict: Iterable[str]):
@@ -44,18 +66,17 @@ def main(*, desigred_dict: Iterable[str]):
 
     logger.info("rcv_usrs_dict : %s", desigred_dict)
 
-    for ip in desigred_dict.values():  # só se conecta nos usuários que vc quer ouvir
-        RECIEVE_SOCKET.connect(f"tcp://{ip}:{DEFAULT_PORT}")
+    for name, ip in desigred_dict.items():  # só se conecta nos usuários que vc quer ouvir
+        # TODO: fix topic_filter
+        topic_filter = b""
+        t = Thread(target=recv_routine, args=(name, ip, topic_filter))
+        t.start()
+        LISTEN_THREADS.append(t)
 
-    # for val in LISTEN_TOPIC_FILTERS.values():
-    #     RECIEVE_SOCKET.setsockopt(zmq.SUBSCRIBE, val)  # pylint: disable=E1101
-
-    RECIEVE_SOCKET.setsockopt(zmq.SUBSCRIBE, BROADCAST_TOPIC)
 
     # TESTS:
-    msg = "eae2" # trocar wrapper kagado
-    SEND_SOCKET.send(f"{BROADCAST_TOPIC} {msg}".encode("utf-8"))
-    print(RECIEVE_SOCKET.recv())
+    while msg := input():
+        SEND_SOCKET.send(f"{BROADCAST_TOPIC} {msg}".encode("utf-8")) # AQUI ENVIA
 
 
 if __name__ == "__main__":
@@ -99,9 +120,9 @@ if __name__ == "__main__":
 
     desigred_dict_input = {}
     for id_ in desigred_conn_ids:
-        name = available_usrs[id_]
-        desigred_dict_input[name] = usr_tbl_dict[name]
+        name_ = available_usrs[id_]
+        desigred_dict_input[name_] = usr_tbl_dict[name_]
 
-    logger.info("desigred_dict: %s", desigred_dict_input)
+    # logger.info("desigred_dict: %s", desigred_dict_input)
 
     main(desigred_dict=desigred_dict_input)
